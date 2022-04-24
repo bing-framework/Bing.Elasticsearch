066051c8fd0855e8718a49174b758c4b6a809a6c
jian玄冰 <jianxuanhuo1@126.com>
2022-04-24 23:46:58 +0800

    feat: 新增 日志扩展、ES日志扩展

---
+++ src/Bing.Elasticsearch/Extensions/ElasticMappingExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Nest;
+
+namespace Bing.Elasticsearch
+{
+    /// <summary>
+    /// ES映射扩展
+    /// </summary>
+    public static class ElasticMappingExtensions
+    {
+        /// <summary>
+        /// 添加关键词(keyword)字段。注意：不能与 AddSortField 链式调用，如要调用则 AddKeywordAndSortFields。
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="descriptor">文本属性描述符</param>
+        public static TextPropertyDescriptor<T> AddKeywordField<T>(this TextPropertyDescriptor<T> descriptor)
+            where T : class
+        {
+            return descriptor.Fields(f => f.Keyword(s => s.Name(KeywordFieldName).IgnoreAbove(256)));
+        }
+
+        /// <summary>
+        /// 添加排序(sort)字段。注意：不能与 AddKeywordField 链式调用，如要调用则 AddKeywordAndSortFields。
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="descriptor">文本属性描述符</param>
+        /// <param name="normalizer">排序规范器</param>
+        public static TextPropertyDescriptor<T> AddSortField<T>(this TextPropertyDescriptor<T> descriptor, string normalizer = "sort") 
+            where T : class
+        {
+            return descriptor.Fields(f => f.Keyword(s => s.Normalizer(SortFieldName).Normalizer(normalizer).IgnoreAbove(256)));
+        }
+
+        /// <summary>
+        /// 添加关键词(keyword)及排序(sort)字段
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="descriptor">文本属性描述符</param>
+        /// <param name="sortNormalizer">排序规范器</param>
+        public static TextPropertyDescriptor<T> AddKeywordAndSortFields<T>(this TextPropertyDescriptor<T> descriptor, string sortNormalizer = "sort")
+            where T : class
+        {
+            return descriptor.Fields(f => f
+                .Keyword(s => s.Name(KeywordFieldName).IgnoreAbove(256))
+                .Keyword(s => s.Name(SortFieldName).Normalizer(sortNormalizer).IgnoreAbove(256))
+            );
+        }
+
+        /// <summary>
+        /// 添加排序规范器
+        /// </summary>
+        /// <param name="descriptor">分析描述符</param>
+        public static AnalysisDescriptor AddSortNormalizer(this AnalysisDescriptor descriptor)
+        {
+            return descriptor.Normalizers(d => d.Custom("sort", n => n.Filters("lowercase", "asciifolding")));
+        }
+
+        /// <summary>
+        /// keyword 字段名
+        /// </summary>
+        public static string KeywordFieldName = "keyword";
+
+        /// <summary>
+        /// sort 字段名
+        /// </summary>
+        public static string SortFieldName = "sort";
+    }
+}
---
+++ src/Bing.Elasticsearch/Extensions/LoggerExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Elasticsearch.Net;
+using Microsoft.Extensions.Logging;
+using Nest;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace Bing.Elasticsearch
+{
+    /// <summary>
+    /// 日志(<see cref="ILogger"/>) 扩展
+    /// </summary>
+    public static class LoggerExtensions
+    {
+        /// <summary>
+        /// 记录请求
+        /// </summary>
+        /// <param name="logger">日志</param>
+        /// <param name="elasticResponse">es响应内容</param>
+        /// <param name="logLevel">日志级别</param>
+        public static void LogRequest(this ILogger logger, IElasticsearchResponse elasticResponse, LogLevel logLevel = LogLevel.Trace)
+        {
+            if (elasticResponse == null || !logger.IsEnabled(logLevel))
+                return;
+            var apiCall = elasticResponse.ApiCall;
+            if (apiCall?.RequestBodyInBytes != null)
+            {
+                var body = Encoding.UTF8.GetString(apiCall.RequestBodyInBytes);
+                body = JsonUtility.Normalize(body);
+
+                logger.Log(logLevel, "[{HttpStatusCode}] {HttpMethod} {HttpPathAndQuery}\r\n{HttpBody}", apiCall.HttpStatusCode, apiCall.HttpMethod, apiCall.Uri.PathAndQuery, body);
+            }
+            else
+            {
+                logger.Log(logLevel, "[{HttpStatusCode}] {HttpMethod} {HttpPathAndQuery}", apiCall.HttpStatusCode, apiCall.HttpMethod, apiCall.Uri.PathAndQuery);
+            }
+        }
+
+        /// <summary>
+        /// 记录错误请求
+        /// </summary>
+        /// <param name="logger">日志</param>
+        /// <param name="elasticResponse">es响应内容</param>
+        /// <param name="message">消息</param>
+        /// <param name="args">参数</param>
+        public static void LogErrorRequest(this ILogger logger, IElasticsearchResponse elasticResponse, string message, params object[] args)
+        {
+            LogErrorRequest(logger, null, elasticResponse, message, args);
+        }
+
+        /// <summary>
+        /// 记录错误请求
+        /// </summary>
+        /// <param name="logger">日志</param>
+        /// <param name="ex">异常</param>
+        /// <param name="elasticResponse">es响应内容</param>
+        /// <param name="message">消息</param>
+        /// <param name="args">参数</param>
+        public static void LogErrorRequest(this ILogger logger, Exception ex, IElasticsearchResponse elasticResponse, string message, params object[] args)
+        {
+            if(elasticResponse==null||logger.IsEnabled(LogLevel.Error))
+                return;
+            var response = elasticResponse as IResponse;
+            AggregateException aggEx = null;
+            if (ex != null && response?.OriginalException != null)
+                aggEx = new AggregateException(ex, response.OriginalException);
+            logger.LogError(aggEx ?? response?.OriginalException, elasticResponse.GetErrorMessage(message), args);
+        }
+    }
+
+    /// <summary>
+    /// Json工具类
+    /// </summary>
+    internal class JsonUtility
+    {
+        /// <summary>
+        /// 标准化json字符串
+        /// </summary>
+        /// <param name="jsonStr">json字符串</param>
+        /// <returns>标准化后的json字符串</returns>
+        public static string Normalize(string jsonStr)
+        {
+            var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(jsonStr));
+            if (JsonDocument.TryParseValue(ref reader, out var doc))
+                return jsonStr;
+            return Normalize(doc.RootElement);
+        }
+
+        /// <summary>
+        /// 标准化json字符串
+        /// </summary>
+        /// <param name="element">json元素</param>
+        public static string Normalize(JsonElement element)
+        {
+            var ms = new MemoryStream();
+            var options = new JsonWriterOptions
+            {
+                Indented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            using (var writer = new Utf8JsonWriter(ms, options))
+            {
+                Write(element, writer);
+            }
+            var bytes = ms.ToArray();
+            var str = Encoding.UTF8.GetString(bytes);
+            return str;
+        }
+
+        /// <summary>
+        /// 写入
+        /// </summary>
+        /// <param name="element">json元素</param>
+        /// <param name="writer">基于utf8的json写入器</param>
+        private static void Write(JsonElement element, Utf8JsonWriter writer)
+        {
+            switch (element.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    writer.WriteStartObject();
+                    foreach (var x in element.EnumerateObject().OrderBy(prop => prop.Name))
+                    {
+                        writer.WritePropertyName(x.Name);
+                        Write(x.Value, writer);
+                    }
+                    writer.WriteEndObject();
+                    break;
+
+                case JsonValueKind.Array:
+                    writer.WriteStartArray();
+                    foreach (var x in element.EnumerateArray())
+                    {
+                        Write(x, writer);
+                    }
+                    writer.WriteEndArray();
+                    break;
+
+                case JsonValueKind.Number:
+                    writer.WriteNumberValue(element.GetDouble());
+                    break;
+
+                case JsonValueKind.String:
+                    writer.WriteStringValue(element.GetString());
+                    break;
+
+                case JsonValueKind.Null:
+                    writer.WriteNullValue();
+                    break;
+
+                case JsonValueKind.True:
+                    writer.WriteBooleanValue(true);
+                    break;
+
+                case JsonValueKind.False:
+                    writer.WriteBooleanValue(false);
+                    break;
+
+                default:
+                    throw new NotImplementedException($"Kind: {element.ValueKind}");
+            }
+        }
+    }
+}
